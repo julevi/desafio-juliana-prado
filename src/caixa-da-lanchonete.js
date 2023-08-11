394a88c39ed200e17a8c90d3993e0ec38ff1690e
Juliana <jprado.contact@gmail.com>
2023-08-11 12:20:05 -0300

    Atribuido o objeto cardápio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,5 +1,40 @@
 class CaixaDaLanchonete {
 
+    cardapioDaLanchonete = {
+        "cafe": {
+            descricao: "Café",
+            valor: 3.00,
+        },
+        "chantily": {
+            descricao: "Chantily (Extra do Café)",
+            valor: 1.50,
+        },
+        "suco": {
+            descricao: "Suco Natural",
+            valor: 6.20,
+        },
+        "sanduiche": {
+            descricao: "Sanduíche",
+            valor: 6.50,
+        },
+        "queijo": {
+            descricao: "Queijo (Extra do Sanduíche)",
+            valor: 2.00,
+        },
+        "salgado": {
+            descricao: "Salgado",
+            valor: 7.25,
+        },
+        "combo1": {
+            descricao: "1 Suco e 1 Sanduíche",
+            valor: 9.50,
+        },
+        "combo2": {
+            descricao: "1 Café e 1 Sanduíche",
+            valor: 7.50,
+        },
+    }
+
     calcularValorDaCompra(metodoDePagamento, itens) {
         return "";
     }
