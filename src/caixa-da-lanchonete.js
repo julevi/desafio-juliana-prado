c80f49fe1580fd44fad2d2adec8bd76102f15316
Juliana <jprado.contact@gmail.com>
2023-08-14 11:18:36 -0300

    Implementada as Regras

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -36,15 +36,69 @@ class CaixaDaLanchonete {
     };
 
     formaDePagamento = {
-        "debito": 1.00,
-        "credito": 1.03,
-        "debito": 0.95,
+        "debito": true,
+        "credito": true,
+        "dinheiro": true,
     };
+
+    desconto(valor, formaDePagamento) {
+        if (formaDePagamento === "dinheiro" || formaDePagamento === "debito") {
+            return valor * 0.05;
+        } else if (formaDePagamento === "credito") {
+            return valor * 0.03;
+        } else {
+            return 0;
+        }
+    }
+
+    verificarItensPrincipais(itens) {
+        for (const item of itens) {
+            const [codigo, _] = item.split(',');
+
+            if ((codigo === "queijo" && !itens.includes("sanduiche")) ||
+                (codigo === "chantily" && !itens.includes("cafe"))) {
+                return "Item extra não pode ser pedido sem o principal";
+            }
+        }
+        return null;
+    }
+
+    naoExisteItens(itens) {
+        if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!";
+        }
+        return null;
+    }
+
+    itemPedidoIgualAZero(itens) {
+        for (const item of itens) {
+            const [codigo, quantidade] = item.split(',');
+
+            if (parseInt(quantidade) === 0) {
+                return "Quantidade inválida!";
+            }
+        }
+        return null;
+    }
+
+    codigoNaoExiste(codigo) {
+        if (!this.cardapioDaLanchonete.hasOwnProperty(codigo)) {
+            return "Item inválido!";
+        }
+        return null;
+    }
+
+    naoExisteFormaDePagamento(formaDePagamento){
+        if (!this.formaDePagamento.hasOwnProperty(formaDePagamento)) {
+            return "Forma de pagamento inválida!";
+        }
+        return null;
+    }
+    
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         return "";
     }
-
 }
 
 export { CaixaDaLanchonete };
